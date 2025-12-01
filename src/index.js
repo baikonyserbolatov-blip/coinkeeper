12122eca782dae97d5e3278024eff1f7b8b8b5af
baikonyserbolatov-blip <baikonyserbolatov@gmail.com>
2025-12-01 11:16:45 +0500

    Refactor index.js to include routing and context

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './auth/AuthContext';
+import './styles.css';
+import App from './App';
+import { Toaster } from 'react-hot-toast';
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+        <Toaster position="top-right" />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
