e1b9eabb34dd6b2b1ba773765cff8e81e482e2ab
baikonyserbolatov-blip <baikonyserbolatov@gmail.com>
2025-12-01 08:48:53 +0500

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="container header-inner">
+        <h1>CoinKeeper</h1>
+        <p className="subtitle">Personal Finance Tracker — учёт доходов и расходов</p>
+      </div>
+    </header>
+  );
+}
